01c1d5ffb7e98216c82b77d622e28a9efa33ac2e
CastilloBryan24 <bryan.castillo24@hotmail.com>
2021-03-25 10:30:12 +0100

    mailing

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,14 @@
-<form action="/mail/store" method="POST" class="mt-5">
-    @csrf
-    <label for="">Email</label>
-    <input type="text">
-    <label for="">Message</label>
-    <input type="text">
-    <button type="submit">Envoyez</button>
-</form>
+<div class="container">
+    <form action="/mail/store" method="POST" class="mt-5">
+        @csrf
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Message</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
